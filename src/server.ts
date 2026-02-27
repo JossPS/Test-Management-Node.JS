d4715e8576f7abf9ccaf59e2d8d0153913f63ba3
Josselyn Simbaña <https://gitlab.com/JosselynSimbana>
2026-02-26 20:07:09 -0500

    Refactor database connection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { Request, Response } from 'express'; 
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { sequelize } from './config/database';
 
 dotenv.config();
 const app = express();
@@ -9,10 +10,23 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ status: 'API is running!' });
+app.get('/', (_req: Request, res: Response) => {
+    res.json({ status: 'La api esta corriendo'});
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync(); 
+        console.log('Conectado a la base de datos');
+        
+        app.listen(PORT, () => {
+                console.log(`Servidor corriendo en el puerto ${PORT}`);
+        });
+    }catch (error) {
+        console.error('Error al conectar con el servidor');
+        process.exit(1);
+    }
+}
+
+startServer();
